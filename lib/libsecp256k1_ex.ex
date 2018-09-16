1325e46debde04d0c004c4eb897b82ee8f6fc0db
Ayrat Badykov <ayratin555@gmail.com>
2018-09-16 18:17:18 +0300

    initial commit

---
+++ lib/libsecp256k1_ex.ex
@@ -0,0 +1,18 @@
+defmodule Libsecp256k1Ex do
+  @moduledoc """
+  Documentation for Libsecp256k1Ex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Libsecp256k1Ex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/libsecp256k1_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule Libsecp256k1ExTest do
+  use ExUnit.Case
+  doctest Libsecp256k1Ex
+
+  test "greets the world" do
+    assert Libsecp256k1Ex.hello() == :world
+  end
+end
